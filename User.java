98b5868fed0beb5c6cf97e1aa73b5d3fff26c20f
Roee Sivan <roee.sivan@post.runi.ac.il>
2025-01-05 18:46:11 +0200

    foo

--- User.java
+++ User.java
@@ -65,7 +65,7 @@
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
         //// Replace the following statement with your code //foo and Foo
-        if((fCount==maxfCount)||(!this.follows(name.toLowerCase()))) //follow list is full
+        if((fCount==maxfCount)||(this.follows(name.toLowerCase()))) //follow list is full
         {
             return false;
         }
