72e79330129a4d12c168543e9faa33eccb7cb67d
Roee Sivan <roee.sivan@post.runi.ac.il>
2025-01-05 18:35:57 +0200

    foo and foo problems

--- Network.java
+++ Network.java
@@ -28,12 +28,7 @@ public class Network {
      *  If there is no such user, returns null.
      *  Notice that the method receives a String, and returns a User object. */
     public User getUser(String name) {
-        //// Replace the following statement with your code
-        if(name ==null)
-        {
-            return null;
-        }
-        
+        //// Replace the following statement with your code        
         for(int i =0;i<userCount;i++)
         {
             if((users[i]!=null)&&users[i].getName().equals(name.toLowerCase()))
--- User.java
+++ User.java
@@ -49,7 +49,6 @@
     if (name == null) {
         return false;
     }
-
     // Loop through the follows array to check for a match
     for (int i = 0; i < fCount; i++) {
         // Check if the current element is not null and equals the given name
@@ -65,8 +64,8 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        //// Replace the following statement with your code
-        if((fCount==maxfCount)||(follows(name.toLowerCase()))) //follow list is full
+        //// Replace the following statement with your code //foo and Foo
+        if((fCount==maxfCount)||(!this.follows(name.toLowerCase()))) //follow list is full
         {
             return false;
         }
